0405da3c4789836f8cbef6d6f4ea0f3822d15910
pdrum <pedram.esmaeeli@gmail.com>
2019-07-18 18:41:51 +0430

    feat: Solve two strings problem

---
+++ maps/README.md
@@ -0,0 +1,2 @@
+My solutions to [arrays and hash maps problems](https://www.hackerrank.com/interview/interview-preparation-kit/dictionaries-hashmaps)
+in hackerrank's interview preparation kit.
---
+++ maps/twostrings/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the twoStrings function below.
+func twoStrings(s1 string, s2 string) string {
+	firstChars := map[int32]interface{}{}
+	for _, char := range s1 {
+		firstChars[char] = true
+	}
+	for _, char := range s2 {
+		_, ok := firstChars[char]
+		if ok {
+			return "YES"
+		}
+	}
+	return "NO"
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	qTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for qItr := 0; qItr < int(q); qItr++ {
+		s1 := readLine(reader)
+
+		s2 := readLine(reader)
+
+		result := twoStrings(s1, s2)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
